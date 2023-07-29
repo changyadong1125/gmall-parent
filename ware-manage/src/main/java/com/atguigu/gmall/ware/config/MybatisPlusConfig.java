ef57c88f63176905ba094a6b5ff9a61f09c6c2e0
FEIYE <2559449523@qq.com>
2023-07-29 09:18:18 +0800

    退款业务 部分退款后续处理

---
+++ ware-manage/src/main/java/com/atguigu/gmall/ware/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.atguigu.gmall.ware.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author helen
+ * @since 2019/9/23
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.atguigu.gmall.*.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // paginationInterceptor.setLimit(你的最大单页限制数量，默认 500 条，小于 0 如 -1 不受限制);
+        return paginationInterceptor;
+    }
+}
